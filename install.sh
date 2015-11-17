707d09c3ce245976ae81e860d3a59863fe886139
Scott Lowe <scott.code.lowe@gmail.com>
2015-11-17 01:43:21 +0000

    Add script for setting up pdf diff with pdftotext
    The script makes an executable in /usr/local/bin/pdf2txt and adds a
    [diff "pdf"] settings to ~/.gitconfig.

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Setup pdf2txt as a function to do git-diff on PDF files
+echo "#!/bin/bash" > /usr/local/bin/pdf2txt
+echo "pdftotext $1 -" >> /usr/local/bin/pdf2txt
+chmod +x /usr/local/bin/pdf2txt
+echo "[diff \"pdf\"]" >> ~/.gitconfig
+echo "    textconv = pdf2txt" >> ~/.gitconfig
