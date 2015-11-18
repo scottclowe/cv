2a1d6fa33df3475fa93b1a8e7f18fce42d90f3f2
Scott Lowe <scott.code.lowe@gmail.com>
2015-11-18 02:48:56 +0000

    Move pre-commit hook to top of install.sh
    We should do this first because it is more likely to succeed and
    has no downside - I am happy to keep overwriting a pre-existing
    file in .git/hooks/pre-commit

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
+# Setup pre-commit hook which generates README.md
+cp -p pre-commit.sh .git/hooks/pre-commit
+
 # Setup pdf2txt as a function to do git-diff on PDF files
 echo "#!/bin/bash" > /usr/local/bin/pdf2txt
 echo "pdftotext $1 -" >> /usr/local/bin/pdf2txt
 chmod +x /usr/local/bin/pdf2txt
 echo "[diff \"pdf\"]" >> ~/.gitconfig
 echo "    textconv = pdf2txt" >> ~/.gitconfig
-# Setup pre-commit hook which generates README.md
-cp -p pre-commit.sh .git/hooks/pre-commit
