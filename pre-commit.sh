f93053e60b39d0a871492c7ac7efc12a1c3c758c
Scott Lowe <scott.code.lowe@gmail.com>
2015-11-18 02:44:06 +0000

    Add verboseness variable
    Controls which messages are echoed to the terminal.

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,12 +2,16 @@
 
 SOURCE=cv.tex
 TARGET=README.md
+VERBOSENESS=1
 
 # Check if SOURCE file has staged changes
 if ! git diff --name-only --cached | grep -q $SOURCE;
 then
     # SOURCE is not staged, so there is nothing to do
-    echo "Source file $SOURCE is not staged, so you're fine."
+    if [ "$VERBOSENESS" -ge "2" ];
+    then
+        echo "Source file $SOURCE is not staged, so there is nothing to do.";
+    fi
     exit 0;
 fi
 
@@ -15,32 +19,46 @@ fi
 if git diff --name-only --cached | grep -q $TARGET;
 then
     # SOURCE is not staged, so there is nothing to do
-    echo "Target file $TARGET has staged changes, so we can't proceed."
-    echo "Either commit changes to $TARGET on its own, or unstage $TARGET."
+    echo "Target file $TARGET has staged changes, so we can't proceed.";
+    echo "You can only stage changes to one of $SOURCE and $TARGET at once.";
     exit 1;
 fi
 
 # Check if SOURCE or TARGET file has unstaged changes
 NEED_STASH=`git ls-files -m | grep -q -e $SOURCE -e $TARGET`
 # If so, we need to temporarily stash changes not on the working index
-if $NEED_STASH
+if $NEED_STASH;
 then
-    echo "Had to stash before working on pre-commit hook"
     git stash -k -q
+    if [ "$VERBOSENESS" -ge "1" ];
+    then
+        echo "Stashed unstaged changes before working on pre-commit hook.";
+    fi
 fi
 
-echo "Pre-commit hook has work to do..."
+if [ "$VERBOSENESS" -ge "2" ];
+then
+    echo "Pre-commit hook has work to do.";
+fi
 
-# Do the work
+# Generate target from source
 pandoc -s "$SOURCE" -o "$TARGET" -t markdown_strict --normalize
+# Stage the generated changes
 git add $TARGET
 
-echo "I've regenerated $TARGET and staged it for you."
+if [ "$VERBOSENESS" -ge "1" ];
+then
+    echo "Regenerated and staged $TARGET based on staged changes to $SOURCE.";
+fi
 
-if $NEED_STASH
+# Pop back from the temporary stash
+if $NEED_STASH;
 then
     git stash pop -q
-    echo "Popped the stash back again"
+    if [ "$VERBOSENESS" -ge "1" ];
+    then
+        echo "Popped the stash back again.";
+    fi
 fi
 
 # Exit with success
